0eac1e5722a8d00627d935719366ff415adfa0fd
Mamath68 <mathieu.stamm@gmail.Com>
2023-12-12 16:50:16 +0100

    front fait, partie dessin non fonctionnel

---
+++ Week2/day2/TP/JS/script.js
@@ -0,0 +1,9 @@
+const colors = [
+    black = document.querySelector('.black'),
+    firebrick = document.querySelector('.firebrick'),
+    orange = document.querySelector('.orange'),
+    yellow = document.querySelector('.yellow'),
+    green = document.querySelector('.green'),
+    blue = document.querySelector('.blue'),
+    purple = document.querySelector('.purple')
+]
