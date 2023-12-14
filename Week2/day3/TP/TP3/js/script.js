e94a958a5a2205c9497322eb14abcb78291522f3
Mamath68 <mathieu.stamm@gmail.Com>
2023-12-14 09:36:36 +0100

    renommage de l'id

--- Week2/day3/TP/TP3/js/script.js
+++ Week2/day3/TP/TP3/js/script.js
@@ -1,3 +1,5 @@
+const adresse = document.querySelector('#adresse');
+
 document.querySelector('#get-location').addEventListener('click', () => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
@@ -5,7 +7,7 @@ document.querySelector('#get-location').addEventListener('click', () => {
             fetch(`https://api-adresse.data.gouv.fr/reverse/?lon=${longitude}&lat=${latitude}`)
                 .then((response) => response.json())
                 .then(response => {
-                    document.querySelector('#address').textContent = response.features[0].properties.label;
+                    adresse.textContent = response.features[0].properties.label;
                 })
 
         });
