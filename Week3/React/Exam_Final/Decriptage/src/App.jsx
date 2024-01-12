9f9af75fbd39e41fd06832db8cb689b8260016a8
Mamath68 <mathieu.stamm@gmail.Com>
2024-01-12 13:36:57 +0100

    new settings file

--- Week3/React/Exam_Final/Decriptage/src/App.jsx
+++ Week3/React/Exam_Final/Decriptage/src/App.jsx
@@ -1,8 +1,8 @@
-import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Layout from "./components/Layout.jsx";
 import Home from './components/Home.jsx';
 import Decriptage from './components/Decriptage.jsx';
+import './App.css';
 
 function App() {
     return (
